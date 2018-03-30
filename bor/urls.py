881004308e92664f7cb14e5e616572aff25f8735
Alex <sss0991@gmail.com>
2018-03-30 21:25:21 +1000

    Создаем модели и их обвязку

---
+++ bor/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='bor-index'),
+
+    url(r'^quotes/$', views.QuotesAllListView.as_view(), name='quotes-all'),
+    url(r'^quote/(?P<pk>\d+)$', views.QuoteDetailView.as_view(), name='quote-detail'),
+
+    url(r'^quote/(?P<pk>\d+)/rulez/$', views.quote_rulez, name='quote-rulez'),
+    url(r'^quote/(?P<pk>\d+)/sux/$', views.quote_sux, name='quote-sux'),
+
+    url(r'^comment/(?P<pk>\d+)$', views.CommentDetailView.as_view(), name='comment-detail'),
+]
--- bor/views.py
+++ bor/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
-# Create your views here.
+from datetime import datetime
+
+from .models import Quote, Comment
+
+
+def index(request):
+    count_quotes_all = Quote.objects.all().count()
+    today_min = datetime.combine(datetime.today(), datetime.min.time())
+    today_max = datetime.combine(datetime.today(), datetime.max.time())
+    count_quotes_new = Quote.objects.filter(date__range=(today_min, today_max)).count()
+    return render(
+        request,
+        'bor/index.html',
+        context={'count_quotes_all': count_quotes_all, 'count_quotes_new': count_quotes_new},
+    )
+
+
+def change_rating(request, pk, val):
+    quote = get_object_or_404(Quote, pk = pk)
+    quote.rating += val
+    quote.save()
+    return redirect('quotes-all')
+
+
+def quote_rulez(request, pk):
+    return change_rating(request, pk, 1)
+
+
+def quote_sux(request, pk):
+    return change_rating(request, pk, -1)
+
+
+class QuoteDetailView(generic.DetailView):
+    model = Quote
+    context_object_name = 'quote'
+    template_name = 'bor/quote_detail.html'
+
+
+class QuotesAllListView(generic.ListView):
+    model = Quote
+    queryset = Quote.objects.order_by('id')
+    context_object_name = 'quotes'
+    template_name = 'bor/quotes_all_list.html'
+
+
+class CommentDetailView(generic.DetailView):
+    model = Comment
+    context_object_name = 'comment'
+    template_name = 'bor/comment_detail.html'
